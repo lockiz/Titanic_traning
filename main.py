0bbe6f3711ce89bcb5b640ec68c64ee932e13c42
evgenij <jenkz1@yandex.ru>
2022-11-20 12:34:57 +0300

    [UPD] Обновил код, на более лучший код

--- main.py
+++ main.py
@@ -11,26 +11,13 @@ def get_free_museum_dates(year):
         year - год (int)
 
     """
+
     for month in range(1, 13):
-        cal = calendar.monthcalendar(year, month)
-        if cal[0].index(1) <= 3:
-            print(date(year, month, cal[0][3] + 14).strftime('%d.%m.%Y'))
-        elif cal[0].index(1) >= 4:
-            print(date(year, month, cal[1][3] + 14).strftime('%d.%m.%Y'))
+        dates_month = calendar.monthcalendar(year, month)
+        if dates_month[0][calendar.THURSDAY]:
+            print(date(year, month, dates_month[2][calendar.THURSDAY]).strftime('%d.%m.%Y'))
         else:
-            print(date(year, month, cal[1][3] + 8).strftime('%d.%m.%Y'))
+            print(date(year, month, dates_month[3][calendar.THURSDAY]).strftime('%d.%m.%Y'))
 
 
 get_free_museum_dates(int(input()))
-
-
-#  Хотел оставить тут комментарий
-
-
-def get_my_input(inp):
-    """ Возвращаем новое * 2 значение из ввода """
-    return inp * 2
-
-
-print(get_my_input(int(input())))
-
